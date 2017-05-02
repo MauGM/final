0b9a837e67df1abd91c52c6b48ad22f8842a00a6
MauGM <maurigm@me.com>
2017-05-02 14:17:01 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("img").on("mouseenter", function(){
+    $(this).removeClass("darken");
+  });
+  $("img").on("mouseleave", function(){
+    $(this).addClass("darken");
+  });
+});
+$(document).ready(function(){
+  $("body").fadeIn(2000);
+$(function(){ 
+
+  
